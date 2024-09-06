83de9dd519fb11ec992297ce5e7d5bb6c60f6d01
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-05 19:48:24 -0600

    failure x2

--- 04-store-api/starter/middleware/error-handler.js
+++ 04-store-api/starter/middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = (err,req,res,next) => {
-    console.log(err)
+    console.log('if the handler executed, this would print', err)
     return res.status(500).json({msg: `Something went wrong, please try again.`})
 }
 
