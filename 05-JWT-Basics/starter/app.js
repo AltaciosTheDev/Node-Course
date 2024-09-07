2cd4a3c2b228e13d8d0ee8e8c6515a9779a1b935
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-07 17:39:42 -0600

    finishing touches to start of project

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,1 +1,34 @@
-console.log('running 05-jwt-token with nodemon')
+require('dotenv').config()
+require('express-async-errors')
+const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const express = require('express')
+
+//app
+const app = express()
+
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+
+//last middleware
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+
+//start server and listen
+const start = async() => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        console.log('connection to db successful')
+        console.log('Server is listening on port 3000')
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start()
