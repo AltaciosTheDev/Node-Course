0e8957fb1dc2fc81f89e4c054e174f6990986b74
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-06 20:03:18 -0600

    query params: company,name

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,13 +2,22 @@ const Product = require('../models/product')
  
 const getAllProductsStatic = async (req,res) => { 
     //throw new Error('Testing express async error package') 
-    const products = await Product.find({featured: true}) 
+    const products = await Product.find(req.query) //empty object makes no difference 
     res.status(200).json({fetched: products.length, data: products}) 
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({msg: 'products route'})
+    const {featured, company} = req.query
+    const queryObject = {}
+    if(featured){ //if property exists at all, enter conditional.
+        queryObject.featured = featured === 'true' ? true : false //ternary checking the value of the featured property.
+    }
+    if(company){
+        queryObject.company = company
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)  
+    res.status(200).json({fetched: products.length, data: products}) 
 }
 
 module.exports = {
