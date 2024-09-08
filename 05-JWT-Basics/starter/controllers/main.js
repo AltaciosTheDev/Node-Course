e85c7c83402bbaed310bb027f248c185415d9d28
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-08 09:44:13 -0600

    easy fixes

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -4,20 +4,21 @@ const CustomAPIError = require('../errors/custom-error')
 const login = async (req, res) => {
     const {username, password} = req.body 
 
+    //if username or password not included throw error, and errorHanlderMiddleware will handle it.
     if(!username || !password){
         throw new CustomAPIError(400, 'Please provide email and password')
     }
     const id = new Date().getDate() //gets the day, which in english it is usually called the date
 
     //try to keep payload small, better experience for user
-    const token = jwt.sign({id,username},process.env.JWT_SECRET, {expiresIn:'30d'})
+    const token = jwt.sign({id,username},process.env.JWT_SECRET, {expiresIn:'30d'}) //info, secret, options
     res.status(200).json({msg: 'user created', token: token})
 }
 
 //with databases 3 options:   
-    //1)moongose validations  
-    //2)Joi (validations)     
-    //3)check in controller   
+    //1)moongose validations  (want to learn with mongoose)
+    //2)Joi (validations)     (used in jobs so i'm interested)
+    //3)check in controller   (used to doing it in controller)
 
 const dashboard = async (req, res) => {
     console.log(req.user)
--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -2,23 +2,24 @@ const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error')
 
 const authMiddleware = async (req, res, next) => {
-    const authHeader = req.headers.authorization
+    const authHeader = req.headers.authorization //did not know we could access the req.headers
 
     //first check if there is a token with correct format
     if(!authHeader || !authHeader.startsWith('Bearer ')){
         throw new CustomAPIError(401, 'No token provided') //error if no token or bad format
     }
 
+    //get only the token part and leave behind the bearer 
     const token = authHeader.split(' ')[1]
 
     //second check if the token is the correct one
     try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const {id, username} = decoded
-        req.user = {id, username}
-        next()
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)//decide with the token, and the secret provided
+        const {id, username} = decoded //extract info from decoded token, so token and decoded stays here and next middleware gets data
+        req.user = {id, username} //add to the requestm, the user property
+        next()  //pass control over to the next middleware
     }
-    catch{(error)
+    catch{(error)//necessary b/c the jwt.verify throws an error if something goes wrong, it does not always need to be aysnc.
         throw new CustomAPIError(401,'Not authorized to access this route') //error if incorrect token 
     }
 
