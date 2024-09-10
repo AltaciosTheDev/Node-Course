306474465cec007f1b8884a208d2f92e32733a39
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-09 18:49:34 -0600

    finish setup

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('express-async-errors');
+const connectDB = require('./db/connect')
 const express = require('express');
 const app = express();
 
@@ -22,6 +23,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
