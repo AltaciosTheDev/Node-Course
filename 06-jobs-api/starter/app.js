590c49b5c450366e88d70cc6517c2ec6402e9fc5
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-09-09 19:34:17 -0600

    finished routers

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,26 +1,36 @@
 require('dotenv').config();
 require('express-async-errors');
-const connectDB = require('./db/connect')
 const express = require('express');
 const app = express();
+
+//connectDB
+const connectDB = require('./db/connect')
+
+//routers
+const jobsRouter = require('./routes/jobs')
+const authRouter = require('./routes/auth')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+//default middleware
 app.use(express.json()); //without this, req.body not accesible. 
+
 // extra packages
 
 // routes
-app.get('/api/v1', (req, res) => {
-  res.send('jobs api');
-});
+app.get('/api/v1/auth', authRouter)
+app.get('/api/v1/jobs', jobsRouter)
 
+//custom middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+//port
 const port = process.env.PORT || 3000;
 
+//start server
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
