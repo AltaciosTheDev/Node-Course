8516bca4bbd921147612d65a3b536b68f2c7ec37
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-10-02 20:46:16 -0600

    hard coded basic find

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,6 +1,8 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find()
+    res.status(200).json({ nbHits: products.length,data: products}) 
 }
 
 const getAllProducsts = async (req, res) => {
