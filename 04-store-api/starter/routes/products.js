2dfad31a549a6d633b422d75091f86a2b06d2d1c
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-10-02 18:35:32 -0600

    delete to recap

--- 04-store-api/starter/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {getAllProductsStatic, getAllProducts} = require('../controllers/products')
-
-const router = express.Router()
-
-router.route('/').get(getAllProducts)
-router.route('/static').get(getAllProductsStatic)
-
-module.exports = router
