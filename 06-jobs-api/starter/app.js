02a25ea7709250219048bd9ff8176195c70bb224
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-10-10 19:49:04 -0600

    routers

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -3,6 +3,13 @@ require('express-async-errors');    //to wrap around all controllers and handle 
 const express = require('express'); //import express
 const app = express();              //create express app
 
+//connectDB
+const connectDB = require('./db/connect')
+
+//import routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 // import custom middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -13,9 +20,8 @@ app.use(express.json()); //if not parsed, will appear undefined due to format
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 //use custom middleware after routers
 app.use(notFoundMiddleware);
