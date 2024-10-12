6da134391d7151812ab7f7cb669ace843d8d290b
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-10-12 14:03:29 -0600

    hash password in controler

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,19 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const bcrypt = require('bcryptjs')
+
+
+
 const register = async (req, res) => {
-    res.send('register user')
+    const {name, email, password} = req.body //extract payload
+
+    const salt = await bcrypt.genSalt(10) //generates salt
+    const hashPassword = await bcrypt.hash(password, salt)
+    const tempUser = {name, email, password: hashPassword}
+
+    const user = await User.create({...tempUser})
+    res.status(StatusCodes.CREATED).json({user}) //201
 }
 
 const login = async ( req, res ) => {
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = required('mongoose')
+const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -9,15 +9,14 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
         type: String,
-        required: [true, 'Please provide name'],,
+        required: [true, 'Please provide name'],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please prpovide valid email"],
-        unique: true
+        //unique: true
     },
     password:{
         type: String,
         required: true,
-        minlength:6,
-        maxlength:12
+        minlength:6
     }
 })
 
