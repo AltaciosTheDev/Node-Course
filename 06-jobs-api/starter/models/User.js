6d3584d5c89a6ccbaa6a55676a0780c626003636
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-10-17 19:48:50 -0600

    created tailored duplicate errors in mongoose

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide name'],
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please prpovide valid email"],
-        unique: false
+        unique: true
     },
     password:{
         type: String,
