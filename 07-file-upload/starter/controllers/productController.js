4cd2d8315b7c6aa9a6c304bee52679f91d282e77
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-12-04 20:21:00 -0600

    express-fileupload library setup and controller

--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -2,7 +2,9 @@ const Product = require('../models/Product')
 const {StatusCodes} = require('http-status-codes')
 
 const createProduct = async (req,res) => {
-    res.send('create product')
+    console.log(req.body)
+    const product = await Product.create(req.body)//create record in db
+    res.status(StatusCodes.CREATED).json({product}) //send status code and info to user
 }
 
 const getAllProducts = async (req, res) => {
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,13 @@
+const path = require('path')
 const {StatusCodes} = require('http-status-codes')
 
 const uploadProductImage = async (req,res) => {
-    res.send('upload product image')
+    let productImage = req.files.image //giant object of library 
+    console.log(__dirname)
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`) //__dirname = current file, and imagePath where to store image
+    console.log(imagePath)
+    await productImage.mv(imagePath)
+    return res.status(StatusCodes.OK).json({image:{src: `/uploads/${productImage.name}`}})
 }
 
 module.exports = {
