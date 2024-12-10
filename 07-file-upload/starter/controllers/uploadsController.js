fd228541db2e67bc367bd3357c64268fd80943a6
AltaciosTheDev <enzo.altamirano98@gmail.com>
2024-12-09 19:42:18 -0600

    additional checks for image presence, image file, image size

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,26 @@
 const path = require('path')
 const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+
 
 const uploadProductImage = async (req,res) => {
-    let productImage = req.files.image //giant object of library 
+    console.log(req.files)
+    if(!req.files){
+        throw new CustomError.BadRequestError('No file uploaded')
+    }
+
+    const productImage = req.files.image //giant object of library 
+
+    if(!productImage.mimetype.startsWith('image')){
+        throw new CustomError.BadRequestError('please upload an image')
+    }
+
+    maxSize = 1024 * 1024
+
+    if(productImage.size > maxSize){
+        throw new CustomError.BadRequestError('please upload image smaller than 1kb')
+    }
+
     console.log(__dirname)
     const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`) //__dirname = current file, and imagePath where to store image
     console.log(imagePath)
